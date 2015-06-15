c499c6f251367266ed57338d499d9d845eae41af
Boy Zelen <boyzelen@gmail.com>
2015-06-15 12:36:44 +0200

    design
    battle screen + actionbar

---
+++ www/js/animations.js
@@ -0,0 +1,17 @@
+$("document").ready(function{
+
+	function startSwipe(){
+		
+		$("#startbg").animate({
+		    opacity: 0.25,
+		    height: "toggle"
+		  }, 5000, function() {
+		    // Animation complete.
+		});
+	}
+	
+	function goBack() {
+    	window.history.back();
+	}
+
+});
