4c924b05958313dfaf21efff0305e3738fac446e
DC0015 <dylan.cremers@gmail.com>
2015-06-18 04:09:41 +0200

    ik ben moe en ga naar bed. MIA bekijkt het maar effe

--- www/js/controller.js
+++ www/js/controller.js
@@ -2,7 +2,14 @@
 var colorValue = 0;
 var starter;
 var duration = 300;
+var lat;
+var long;
 
+/*function geoCheckReady () {
+			
+			navigator.geolocation.getCurrentPosition(geoCheck);
+			alert('voert hij dit nog uit?');
+		}*/
 function vibrate() {
         navigator.notification.vibrate(2000);
     }
@@ -18,6 +25,7 @@ function selectStarter(element, color) {
 		document.getElementById('button').style.visibility = 'visible';
 		colorValue = 1;
 		console.log(starter);
+		
 		
 	}
 	else
@@ -36,14 +44,15 @@ function caughtPokemon() {
 }*/
 
 angular.module('starter.controllers', [])
-.controller('pokedexCtrl', function($scope, $stateParams, $ionicPopup, $timeout) {
+.controller('pokedexCtrl', function($scope, $stateParams, $ionicPopup, $timeout, $state) {
 	//var bulba = document.getElementById("bulba").value;
 	//alert('it works');
 	//alert(colorValue);
 	alert(starter);
+	
 	if (starter == 'bulba')
 	{
-		alert('it works!');
+		//alert('it works!');
 		document.getElementById('caughtBulba').style.visibility = 'visible';
 	}
 	else if(starter == 'char')
@@ -77,17 +86,62 @@ angular.module('starter.controllers', [])
 			templete: '',
 			title: '<b>Warning</b>',
 			subTitle: 'A wild Pokémon appeared',
+			scope: $scope,
 			buttons: 
 			[{
-				text: 'Battle'
+				text: 'Battle',
+				onTap: function(){
+					//alert('I enter the function');
+					$state.go('battle');
+				}
 			}]
 			}); }, 5000);
+		document.addEventListener("deviceready", onDeviceReady, false)
 		
+
+    // device APIs are available
+    //
+	setInterval(onDeviceReady, 100);
+    function onDeviceReady() {
+        navigator.geolocation.getCurrentPosition(onSuccess, onError);
+		//navigator.geolocation.getCurrentPosition(geoCheck);
+    }
+
+    // onSuccess Geolocation
+    //
+    function onSuccess(position) {
+        var element = document.getElementById('geolocation');
+        element.innerHTML = 'Latitude: '           + position.coords.latitude              + '<br />' +
+                            'Longitude: '          + position.coords.longitude             + '<br />';
+		
+		lat = position.coords.latitude + 0.000333;
+		alert(position.coords.latitude +" "+lat);
+		long = position.coords.longitude;
+		//alert(lat);
+		//lat = lat + 0.000333;
+		
+		//alert(long);
+		long = long - 0.00079;
+		//alert(long);
+		/*function geoCheck() {
+			alert(lat + "What'choo talkin' 'bout, Willis?");
+		}*/
+		/*return lat;
+		return long;*/
+		/*setInterval*//*(function geoCheck()
+	{
+		alert(position.coords.latitude + " "+ lat);
+	}, 100);*/
+		
+    }
+
+	
+    // onError Callback receives a PositionError object
+    //
+    function onError(error) {
+        alert('code: '    + error.code    + '\n' +
+              'message: ' + error.message + '\n');
+    }
+	//geoCheckReady();
 });
 
-/*function MyCtrl($scope, $ionicHistory) {
-  $scope.myGoBack = function() {
-    $ionicHistory.goBack();
-  };
-}
-*/
